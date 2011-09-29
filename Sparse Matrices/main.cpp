f04c5e4ba3ac49b4ae110f33e84b70633932b7b1
Dominik Veselý <vesely@dominoo.cz>
2011-09-29 02:02:41 +0200

    Initial Commit

---
+++ Sparse Matrices/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sparse Matrices
+//
+//  Created by Dominik Veselý on 9/29/11.
+//  Copyright (c) 2011 CTU. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
